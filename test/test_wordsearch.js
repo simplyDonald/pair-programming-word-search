cef82ee24d4940999930a9b42c0685112aa6edab
simplyDonald <donaldabuah@gmail.com>
2021-11-03 22:13:17 +0000

    completed PP with @Jonathan Afong

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,64 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return true if the word is vertically present", function() {
+    const result = wordSearch([
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return true if the word is reversed & vertically present", function() {
+    const result = wordSearch([
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['K', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['C', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['O', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ROCK')
+
+    assert.isTrue(result);
+  });
+  
+  it("should return true if the word is reversed & horizontally present", function() {
+    const result = wordSearch([
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['K', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['C', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['O', 'F', 'L', 'A', 'T', 'E', 'M', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'METAL')
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if the array is empty", function() {
+    const result = wordSearch([], false)
+
+    assert.isFalse(result);
+  });
+  
+  it("should return false if the array is empty", function() {
+    const result = wordSearch(undefined, false)
+
+    assert.isFalse(result);
+  });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,55 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function(matrix) {
+    // Put your solution here
+  const newMatrix = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    let elementArray = [];
+      for (let j = 0; j < matrix.length; j++) {
+        //console.log(matrix[i][j]);
+      elementArray.push(matrix[j][i]);
+     }
+     newMatrix.push(elementArray);
+  };
+  
+   return newMatrix;
+  };
+  
+//   const diaTranspose = (matrix) => {
+//     const newMatrix = [];
+//     //loop matrix and push diagonally i = hori, j = verti
+//     for (let i = 0; i < matrix[0].length; i++) {
+//         //create diagonal slots
+//         let elementArray = [];
+//         for (let j = 0; j < matrix.length; j++) {
+//             //
+//             //matrix.map((el, index) => el[index]);
+//             //elementArray.push(matrix[][]); //[row ][column + 1] elements have to be incremented by 1 at the same time?
+            
+//         }
+//         //push into diagonal slots
+//         newMatrix.push(elementArray);
+//     };
+//     return newMatrix;
+//   };
 
-module.exports = wordSearch
+    const wordSearch = (letters, word) => { 
+    if (letters === undefined || letters.length === 0  || word === undefined || word === false) {
+        return false; 
+    }
+    let reverseWord = word.split('').reverse().join('');
+    //console.log(reverseWord);
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let newMatrix = transpose(letters);
+    const verticalJoin = newMatrix.map(ls => ls.join(''));
+    for (l of horizontalJoin) {
+        if (l.includes(word) || l.includes(reverseWord)) return true
+    };
+    for (l of verticalJoin) {
+        if (l.includes(word) || l.includes(reverseWord)) return true
+    };
+    return false;
+  };
+  
+  
+  
+  module.exports = wordSearch
+  
